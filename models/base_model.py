cfb5a2f295c06a7072a0ad101880ec54580556cd
TheOneNyangweso <morurisammy5@gmail.com>
2024-06-27 20:17:24 +0300

    test

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,33 @@
+#!/usr/bin/python3
+"""Defines a base class to be used by all models
+"""
 from datetime import datetime
 import uuid
 
 
 class BaseModel:
+    """Class that defines properties of base
+    """
+
     def __init__(self):
+        """Creates new instances of Base
+        """
         self.id: str = str(uuid.uuid4())
         self.created_at: datetime = datetime.now()
         self.updated_at: datetime = datetime.now()
 
     def save(self):
+        """Update public instance attribute updated_at with current datetime.
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Returns a dictionary containing all key/values of __dict__ of
+        the instance.
+
+        Returns:
+            dict: key/value pairs.
+        """
         model_dict = self.__dict__
         model_dict['__class__'] = self.__class__.__name__
         model_dict['created_at'] = self.created_at.strftime(
@@ -21,4 +37,9 @@ class BaseModel:
         return model_dict
 
     def __str__(self) -> str:
+        """Returns a string represation of class details.
+
+        Returns:
+            str: class details
+        """
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
