335668f4b9b60cd9418bd2f7c3112a1c147b7e6f
TheOneNyangweso <morurisammy5@gmail.com>
2024-06-27 20:44:14 +0300

    ,,,,

--- models/base_model.py
+++ models/base_model.py
@@ -9,12 +9,21 @@ class BaseModel:
     """Class that defines properties of base
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Creates new instances of Base
         """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+
+                setattr(self, key, value)
+
         self.id: str = str(uuid.uuid4())
         self.created_at: datetime = datetime.now()
-        self.updated_at: datetime = datetime.now()
+        self.updated_at: datetime = self.created_at
 
     def save(self):
         """Update public instance attribute updated_at with current datetime.
@@ -32,7 +41,7 @@ class BaseModel:
         model_dict['__class__'] = self.__class__.__name__
         model_dict['created_at'] = self.created_at.strftime(
             '%Y-%m-%dT%H:%M:%S.%f')
-        model_dict['updates_at'] = self.updated_at.strftime(
+        model_dict['updated_at'] = self.updated_at.strftime(
             '%Y-%m-%dT%H:%M:%S.%f')
         return model_dict
 
